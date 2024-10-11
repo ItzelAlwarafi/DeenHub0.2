3d9d957b6055d026205e76bf33f0570abf4b185f
itzel <itzelalwarafii@gmail.com>
2024-10-10 18:42:58 -0700

    prayerTracker component

--- client/src/components/PrayerTrackerComp.jsx
+++ client/src/components/PrayerTrackerComp.jsx
@@ -1,12 +1,48 @@
+import { MdCheckBox, MdCheckBoxOutlineBlank } from "react-icons/md" // Import checked and unchecked icons
+import { BsFillSunriseFill, BsSunsetFill } from "react-icons/bs"
+import { MdSunny, MdOutlineNightlightRound } from "react-icons/md"
+import { useState } from "react"
 
+const iconMapping = { // Icon for prayers
+    Fajr: <BsFillSunriseFill />,
+    Dhuhr: <MdSunny />,
+    Asr: <MdSunny />,
+    Maghrib: <BsSunsetFill />,
+    Isha: <MdOutlineNightlightRound />,
+};
 
+export default function PrayerTrackerComp() {
+    const prayerNames = ['Fajr', 'Dhuhr', 'Asr', 'Maghrib', 'Isha']
+    const [checkedPrayers, setCheckedPrayers] = useState({}) 
 
-export default function PrayerTrackerComp (){
+    const handleCheckboxChange = (prayer) => {
+        setCheckedPrayers((prev) => ({
+            ...prev,
+            [prayer]: !prev[prayer], // Toggle the checked state
+        }))
+    }
 
     return (
         <div className="Prayertracker-pagecontainer">
-        <h1>prayer tracker component</h1>
-        
+            <h1 className="prayer-Tracker-Title"> Prayer Tracker</h1>
+             <div className="Prayertracker-prayers-container">
+            {prayerNames.map((prayerName) => (
+                <div key={prayerName} className="prayer-item">
+                    <span onClick={() => handleCheckboxChange(prayerName)}>
+                        {checkedPrayers[prayerName] ? (
+                            <MdCheckBox className="prayer-checkbox" />
+                        ) : (
+                            <MdCheckBoxOutlineBlank className="prayer-checkbox" />
+                        )}
+                    </span>
+                    <h1 className="prayer-tracker-icon">{iconMapping[prayerName]}</h1>
+                    <h1 className="prayer-tracker-name">{prayerName}</h1>
+                  
+                </div>
+            ))}
+            </div>
         </div>
     )
 }
+
+
